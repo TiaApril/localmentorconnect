5ce285ec319ba9ce5ffa982adf267cf9b168ca14
tiana <tianaaprianti85@gmail.com>
2023-04-08 22:50:08 +0700

    commit

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div className="landingpage">
+        <div className="navbar">
+            <div className="logo">
+                <h1>LocalMentorConnect</h1>
+            </div>
+            <div className="navbarbutton">
+                <button>Log In</button>
+                <button>Sign In</button>
+                <button>Demo</button>
+            </div>
+        </div>
+        <div className="hero"></div>
+    </div>
+  )
+}
+
+export default LandingPage
